aa5370af8d14ed28e561f2548f81036d10dc2582
shivchauhan1012koch <shivpratapsingh.chauhan@kochcc.com>
2024-09-25 22:08:15 +0530

    Tech Fiesta_1

--- src/main/java/Page_Objects/Project_Creation.java
+++ src/main/java/Page_Objects/Project_Creation.java
@@ -52,7 +52,7 @@ public class Project_Creation extends Setup {
         wait.until(ExpectedConditions.elementToBeClickable(ProjectCreated));
         WebElement ProjectNameVerification = driver.findElement(ProjectCreated);
         String msg = ProjectNameVerification.getText();
-        String expectedMsg = "Completed : Project created successfully with Id : 2424211111";
+        String expectedMsg = "Completed : Project created successfully with Id : 24242111111";
 //        Assert.assertEquals(msg, expectedMsg);
         Assert.assertEquals(msg, expectedMsg);
     }
